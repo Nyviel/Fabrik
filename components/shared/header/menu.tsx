75e339d6de7a72a6729aa6487a3f20a3217ad666
Przemysław Kaczmarski <nyviel111@gmail.com>
2025-03-25 13:04:03 +0100

    Header component

---
+++ components/shared/header/menu.tsx
@@ -0,0 +1,54 @@
+import { Button } from "@/components/ui/button";
+import { EllipsisVertical, ShoppingCart, UserIcon } from "lucide-react";
+import ThemeSwitcher from "./theme-switcher";
+import Link from "next/link";
+import {
+	Sheet,
+	SheetContent,
+	SheetDescription,
+	SheetTitle,
+	SheetTrigger,
+} from "@/components/ui/sheet";
+
+const Menu = () => {
+	return (
+		<div className="flex justify-end gap-3">
+			<nav className="hidden md:flex w-full max-w-xs gap-1">
+				<ThemeSwitcher />
+				<Button asChild variant="ghost">
+					<Link href="/cart">
+						<ShoppingCart /> Cart
+					</Link>
+				</Button>
+				<Button asChild>
+					<Link href="/sign-in">
+						<UserIcon /> Sign In
+					</Link>
+				</Button>
+			</nav>
+			<nav className="md:hidden">
+				<Sheet>
+					<SheetTrigger className="align-middle">
+						<EllipsisVertical />
+					</SheetTrigger>
+					<SheetContent className="flex flex-col p-6 items-start">
+						<SheetTitle>Menu</SheetTitle>
+						<ThemeSwitcher />
+						<Button asChild variant="ghost">
+							<Link href="/cart">
+								<ShoppingCart /> Cart
+							</Link>
+						</Button>
+						<Button asChild>
+							<Link href="/sign-in">
+								<UserIcon /> Sign In
+							</Link>
+						</Button>
+						<SheetDescription></SheetDescription>
+					</SheetContent>
+				</Sheet>
+			</nav>
+		</div>
+	);
+};
+export default Menu;
---
+++ components/shared/header/theme-switcher.tsx
@@ -0,0 +1,64 @@
+"use client";
+import type { NextPage } from "next";
+import { useTheme } from "next-themes";
+import {
+	DropdownMenu,
+	DropdownMenuCheckboxItem,
+	DropdownMenuContent,
+	DropdownMenuLabel,
+	DropdownMenuSeparator,
+	DropdownMenuTrigger,
+} from "../../ui/dropdown-menu";
+import { Button } from "../../ui/button";
+import { MoonIcon, SunIcon, SunMoon } from "lucide-react";
+import { useEffect, useState } from "react";
+
+const ThemeSwitcher: NextPage = () => {
+	const { theme, setTheme } = useTheme();
+	const [mounted, setMounted] = useState(false);
+
+	useEffect(() => setMounted(true), []);
+
+	if (!mounted) return null;
+
+	return (
+		<DropdownMenu>
+			<DropdownMenuTrigger asChild>
+				<Button variant={"ghost"}>
+					{theme === "system" ? (
+						<SunMoon />
+					) : theme === "dark" ? (
+						<MoonIcon />
+					) : (
+						<SunIcon />
+					)}{" "}
+					Themes
+				</Button>
+			</DropdownMenuTrigger>
+			<DropdownMenuContent>
+				<DropdownMenuLabel>Appearance</DropdownMenuLabel>
+				<DropdownMenuSeparator />
+				<DropdownMenuCheckboxItem
+					checked={theme === "system"}
+					onClick={() => setTheme("system")}
+				>
+					System
+				</DropdownMenuCheckboxItem>
+				<DropdownMenuCheckboxItem
+					checked={theme === "dark"}
+					onClick={() => setTheme("dark")}
+				>
+					Dark
+				</DropdownMenuCheckboxItem>
+				<DropdownMenuCheckboxItem
+					checked={theme === "light"}
+					onClick={() => setTheme("light")}
+				>
+					Light
+				</DropdownMenuCheckboxItem>
+			</DropdownMenuContent>
+		</DropdownMenu>
+	);
+};
+
+export default ThemeSwitcher;

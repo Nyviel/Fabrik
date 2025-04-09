a5493bac35f8c41f58368233165ad83e2056c7f3
Przemysław Kaczmarski <nyviel111@gmail.com>
2025-04-09 19:59:52 +0200

    Styling changes to main root layout

--- app/user/layout.tsx
+++ app/user/layout.tsx
@@ -1,5 +1,6 @@
-import Menu from "@/components/shared/header/menu";
-import UserMainNav from "@/components/shared/user-main-nav/user-main-nav";
+import Footer from "@/components/footer/footer";
+import Menu from "@/components/header/menu";
+import UserMainNav from "@/components/user/user-main-nav";
 import { APP_NAME } from "@/lib/constants";
 import Image from "next/image";
 import Link from "next/link";
@@ -10,7 +11,7 @@ export default function Layout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<div className="flex flex-col">
+		<div className="flex flex-col h-screen">
 			<div className="border-b w-full">
 				<div className="wrapper mx-auto flex items-center h-16 px-4 my-3">
 					<Link href="/" className="w-22">
@@ -30,6 +31,7 @@ export default function Layout({
 			<div className="flex-1 space-y-4 p-8 pt-6 wrapper mx-auto">
 				{children}
 			</div>
+			<Footer />
 		</div>
 	);
 }

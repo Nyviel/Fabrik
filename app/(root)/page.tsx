298e1e0e41eb08354723f3567575d0ad01e13367
Przemysław Kaczmarski <nyviel111@gmail.com>
2025-04-09 19:59:04 +0200

    Added Product carousel

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,16 +1,26 @@
-import ProductList from "@/components/shared/product/product-list";
-import { getLatestProducts } from "@/lib/actions/product.actions";
+import ProductCarousel from "@/components/product/product-carousel";
+import ProductList from "@/components/product/product-list";
+import ViewAllProductsButton from "@/components/view-all-products-button/view-all-products-button";
+import {
+	getFeaturedProducts,
+	getLatestProducts,
+} from "@/lib/actions/product.actions";
 import { LATEST_PRODUCTS_LIMIT } from "@/lib/constants";
 
 const HomePage = async () => {
 	const latestProducts = await getLatestProducts();
+	const featuredProducts = await getFeaturedProducts();
 	return (
 		<div>
+			{featuredProducts.length > 0 && (
+				<ProductCarousel data={featuredProducts} />
+			)}
 			<ProductList
 				data={latestProducts}
 				title="Newest Arrivals"
 				limit={LATEST_PRODUCTS_LIMIT}
 			/>
+			<ViewAllProductsButton />
 		</div>
 	);
 };

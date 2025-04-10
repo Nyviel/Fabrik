af6e56440286eb9df8cced43061a7c3b25a46ced
Przemysław Kaczmarski <nyviel111@gmail.com>
2025-04-10 12:38:44 +0200

    Update banner.tsx

--- components/banner/banner.tsx
+++ components/banner/banner.tsx
@@ -1,6 +1,6 @@
 const Banner = () => {
 	return (
-		<div className="w-full h-[200px] bg-primary text-gray-900 text-lg py-4 text-center">
+		<div className="w-full h-fit bg-primary text-gray-900 text-lg py-4 text-center">
 			This is a demo project, utilized products are fake and so are the
 			payments. Do not use your secure information on this website.
 		</div>
